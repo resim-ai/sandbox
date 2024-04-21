92d21a4ce19d9ecf2ab4c7a668923e23a1888e23
Michael Bauer <michael@resim.ai>
2024-04-21 04:13:52 +0000

    foo

---
+++ systems/cheese/README.md
@@ -0,0 +1,6 @@
+
+# Cheese
+
+This folder contains an incredibly simple example of how to create a build and a
+metrics build that compute different results depending on what sort of cheese is
+present in the experience.
--- systems/cheese/build_and_push.sh
+++ systems/cheese/build_and_push.sh
@@ -23,7 +23,8 @@ _print() {
 }
 
 
-LOCAL_TAG="hello_world:latest"
+LOCAL_TAG="cheese:latest"
+LOCAL_METRICS_TAG="cheese_metrics:latest"
 
 _require RESIM_SANDBOX_ECR
 _require RESIM_SANDBOX_ECR_REPO
@@ -31,8 +32,9 @@ _require RESIM_SANDBOX_ECR_REGION
 _require RESIM_SANDBOX_PROJECT
 _require RESIM_SANDBOX_SYSTEM
 
-_set_default RESIM_SANDBOX_BUILD_TAG_PREFIX "hello_world_"
-_set_default RESIM_SANDBOX_BUILD_BRANCH "hello_world_sandbox"
+_set_default RESIM_SANDBOX_BUILD_TAG_PREFIX "cheese_"
+_set_default RESIM_SANDBOX_METRICS_BUILD_TAG_PREFIX "cheese_metrics_"
+_set_default RESIM_SANDBOX_BUILD_BRANCH "cheese_sandbox"
 _set_default RESIM_SANDBOX_BUILD_VERSION "$(git rev-parse HEAD)"
 
 echo "Running with..."
@@ -52,6 +54,9 @@ cd $(dirname "$0")
 echo "Building the build image..."
 ./build.sh
 
+echo "Building the metrics image..."
+./metrics/build.sh
+
 echo "Performing ECR Login..."
 aws ecr get-login-password --region "${RESIM_SANDBOX_ECR_REGION}" \
     | docker login --username AWS --password-stdin "${RESIM_SANDBOX_ECR}"
@@ -69,5 +74,16 @@ resim builds create \
       --project "${RESIM_SANDBOX_PROJECT}" \
       --system "${RESIM_SANDBOX_SYSTEM}" \
       --version "${RESIM_SANDBOX_BUILD_VERSION}" \
-      --auto-create-branch \
-      --github
+      --auto-create-branch
+
+echo "Pushing metrics image..."
+_FULL_METRICS_TAG="${RESIM_SANDBOX_ECR}/${RESIM_SANDBOX_ECR_REPO}:${RESIM_SANDBOX_METRICS_BUILD_TAG_PREFIX}${RESIM_SANDBOX_BUILD_VERSION}"
+docker tag "${LOCAL_METRICS_TAG}" "${_FULL_METRICS_TAG}"
+docker push "${_FULL_METRICS_TAG}"
+
+echo "Registering build with ReSim..."
+resim metrics-builds create \
+      --name "A ReSim sandbox metrics build." \
+      --image "${_FULL_METRICS_TAG}" \
+      --project "${RESIM_SANDBOX_PROJECT}" \
+      --version "${RESIM_SANDBOX_BUILD_VERSION}"
