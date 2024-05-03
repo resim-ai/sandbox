d50e6fbfd4684c6fbf5b6a12a9b3afab0196ac11
Michael Bauer <michael@resim.ai>
2024-05-03 22:04:24 +0000

    Build working with visualization

--- systems/drone/build.sh
+++ systems/drone/build.sh
@@ -13,9 +13,14 @@ bazel build @resim_open_core//resim/simulator:resim_run
 RESIM_RUN_RUNFILES_DIR="$(bazel info bazel-bin)/external/resim_open_core/resim/simulator/resim_run.runfiles/"
 
 
+bazel build @resim_open_core//resim/visualization/log:make_visualization_log
+MAKE_VISUALIZATION_LOG_RUNFILES_DIR="$(bazel info bazel-bin)/external/resim_open_core/resim/visualization/log/make_visualization_log.runfiles/"
+
+
 popd
 
 cp -Lr "${RESIM_RUN_RUNFILES_DIR}" bin/
+cp -Lr "${MAKE_VISUALIZATION_LOG_RUNFILES_DIR}" bin/
 
 ls bin/
 
--- systems/drone/entrypoint.sh
+++ systems/drone/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+INPUTS="/tmp/resim/inputs"
+OUTPUTS="/tmp/resim/outputs"
+
 pushd /resim/resim_run.runfiles/resim_open_core/
 
 resim/simulator/resim_run \
-    --config /tmp/resim/inputs/experience.sim \
-    --log /tmp/resim/outputs/resim_log.mcap
+    --config "${INPUTS}/experience.sim" \
+    --log "${OUTPUTS}/resim_log.mcap"
+
+popd
+
+pushd /resim/make_visualization_log.runfiles/resim_open_core/
+
+resim/visualization/log/make_visualization_log \
+    --log "${OUTPUTS}/resim_log.mcap" \
+    --output "${OUTPUTS}/vis.mcap"
 
 popd
