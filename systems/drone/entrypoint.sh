8fa4b041d9ec1476481aade238af4c450383a002
Iain Whiteside <iain@resim.ai>
2024-09-26 14:00:50 +0000

    ss

--- systems/drone/entrypoint.sh
+++ systems/drone/entrypoint.sh
@@ -4,7 +4,14 @@
 # Check if the parameters.json file that exists in /tmp/resim/inputs contains the velocity cost override:
 if [ -f /tmp/resim/inputs/parameters.json ]; then
   VELOCITY_COST_OVERRIDE=$(jq -r '.velocity_cost_override' /tmp/resim/inputs/parameters.json)
-  echo "Found a velocity cost override in the parameters.json file: $VELOCITY_COST_OVERRIDE"
+  # check it is not null:
+    if [ "$VELOCITY_COST_OVERRIDE" == "null" ]; then
+        VELOCITY_COST_OVERRIDE=""
+        echo "No velocity cost override found in parameters"
+    # otherwise:
+    else
+        echo "Found a velocity cost override in the parameters.json file: $VELOCITY_COST_OVERRIDE"
+    fi
 fi
 
 echo "Running container with cost override: $VELOCITY_COST_OVERRIDE"
